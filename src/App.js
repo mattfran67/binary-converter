22ab96c07057537b9a03e2797f1030c906af51d0
Mateus Gonçalves Franco <mattfran67@gmail.com>
2021-01-23 17:18:47 -0300

    Acrescentado os primeiros elementos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const handleChange = e => {
+    if (!isNaN(e.target.value)) {
+      setValue(e.target.value)
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        value={value}
+        onChange={handleChange}
+      />
+      <input
+        value={value}
+        onChange={handleChange}
+      />
     </div>
   );
 }
